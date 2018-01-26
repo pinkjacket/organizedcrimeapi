18647fe91da0759b53054fd3d371054428707e09
James Donlan <jacketlizard@live.com>
2018-01-26 12:03:05 -0800

    basic objects settled on, moving to interfaces

---
+++ src/main/java/models/Individual.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.util.Objects;
+
+public class Individual {
+    private String name;
+    private String description;
+    private int id;
+    private int organizationId;
+
+    public Individual(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(int organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Individual that = (Individual) o;
+        return getOrganizationId() == that.getOrganizationId() &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getDescription(), that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getName(), getDescription(), getOrganizationId());
+    }
+}
--- src/main/java/models/Organization.java
+++ src/main/java/models/Organization.java
@@ -1,11 +1,87 @@
 package models;
 
+import java.util.Objects;
+
 public class Organization {
     private String name;
-    private String trueName;
+    private String truename;
     private String location;
-    private int membership;
+    private String membership;
     private String govtrelationship;
     private int id;
 
+    public Organization(String name, String truename, String location, String membership, String govtrelationship) {
+        this.name = name;
+        this.truename = truename;
+        this.location = location;
+        this.membership = membership;
+        this.govtrelationship = govtrelationship;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTruename() {
+        return truename;
+    }
+
+    public void setTruename(String truename) {
+        this.truename = truename;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMembership() {
+        return membership;
+    }
+
+    public void setMembership(String membership) {
+        this.membership = membership;
+    }
+
+    public String getGovtrelationship() {
+        return govtrelationship;
+    }
+
+    public void setGovtrelationship(String govtrelationship) {
+        this.govtrelationship = govtrelationship;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return getId() == that.getId() &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getTruename(), that.getTruename()) &&
+                Objects.equals(getLocation(), that.getLocation()) &&
+                Objects.equals(getMembership(), that.getMembership()) &&
+                Objects.equals(getGovtrelationship(), that.getGovtrelationship());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getName(), getTruename(), getLocation(), getMembership(), getGovtrelationship(), getId());
+    }
 }
